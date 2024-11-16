55e7a9fe285d0269c6e0035fa0fdf0a82377abd4
enyineer <nico.enking@gmail.com>
2024-11-16 19:58:26 +0100

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { FaGithub } from "react-icons/fa";
 import LinkButton from "../components/buttons/linkButton";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <LinkButton href="/game">Go to Game</LinkButton>
+    <div className="h-screen w-screen flex flex-col items-center justify-center gap-8">
+      <h1 className="text-2xl">Welcome to NissesGen!</h1>
+      <div className="flex gap-4">
+        <LinkButton href="/game">Go to Game</LinkButton>
+        <LinkButton
+          href="https://github.com/enyineer/nissesgen/"
+          className="flex gap-2 items-center"
+        >
+          <div>GitHub</div>
+          <FaGithub />
+        </LinkButton>
+      </div>
     </div>
   );
 }
