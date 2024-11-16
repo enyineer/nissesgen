f544496ac4391172c0194532311cb1cddca2d2f4
enyineer <nico.enking@gmail.com>
2024-11-16 23:05:43 +0100

    Improve cost functions

--- engine/useFarmerCalculation.ts
+++ engine/useFarmerCalculation.ts
@@ -1,31 +1,42 @@
 import { BigNumber } from "mathjs";
 import { gameMath } from "../gameMath";
 import { useFarmerStore } from "../stores/farmerStore";
+import { useMemo } from "react";
 
 export const FARMER_UNLOCK_COST = gameMath.bignumber("1e10");
 export const FARMER_MULTIPLIER_PER_LEVEL = gameMath.bignumber("1");
-export const FARMER_MULTIPLIER_COST_PER_LEVEL = gameMath.bignumber("10");
+export const FARMER_MULTIPLIER_BASE_COST = gameMath.bignumber("10");
+export const FARMER_MULTIPLIER_COST_RATE = gameMath.bignumber("3");
 export const FARMER_EXPONENT_PER_LEVEL = gameMath.bignumber("0.1");
-export const FARMER_EXPONENT_COST_PER_LEVEL = gameMath.bignumber("10");
+export const FARMER_EXPONENT_BASE_COST = gameMath.bignumber("10");
+export const FARMER_EXPONENT_COST_RATE = gameMath.bignumber("3");
 
 export function useFarmerCalculation() {
   const farmerStore = useFarmerStore();
 
-  const farmerExponent: BigNumber = gameMath.evaluate(
-    `1 + ((${farmerStore.exponentLevel} - 1) * ${FARMER_EXPONENT_PER_LEVEL})`
-  );
-  const farmerExponentCost: BigNumber = gameMath.evaluate(
-    `${farmerStore.exponentLevel} * ${FARMER_EXPONENT_COST_PER_LEVEL}`
-  );
-  const farmerMultiplier: BigNumber = gameMath.evaluate(
-    `${farmerStore.level} * ${FARMER_MULTIPLIER_PER_LEVEL}`
-  );
-  const farmerMultiplierCost: BigNumber = gameMath.evaluate(
-    `${farmerStore.level} * ${FARMER_MULTIPLIER_COST_PER_LEVEL}`
-  );
-  const farmerTickFactor: BigNumber = gameMath.evaluate(
-    `${farmerMultiplier} ^ ${farmerExponent}`
-  );
+  const farmerExponent: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `1 + ((${farmerStore.exponentLevel} - 1) * ${FARMER_EXPONENT_PER_LEVEL})`
+    );
+  }, [farmerStore.exponentLevel]);
+  const farmerExponentCost: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${FARMER_EXPONENT_BASE_COST} * (${FARMER_EXPONENT_COST_RATE}) ^ ${farmerStore.exponentLevel}`
+    );
+  }, [farmerStore.exponentLevel]);
+  const farmerMultiplier: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${farmerStore.level} * ${FARMER_MULTIPLIER_PER_LEVEL}`
+    );
+  }, [farmerStore.level]);
+  const farmerMultiplierCost: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${FARMER_MULTIPLIER_BASE_COST} * (${FARMER_MULTIPLIER_COST_RATE}) ^ ${farmerStore.level}`
+    );
+  }, [farmerStore.level]);
+  const farmerTickFactor: BigNumber = useMemo(() => {
+    return gameMath.evaluate(`${farmerMultiplier} ^ ${farmerExponent}`);
+  }, [farmerExponent, farmerMultiplier]);
 
   return {
     farmerExponent,
--- engine/useGeneratorCalculation.ts
+++ engine/useGeneratorCalculation.ts
@@ -1,30 +1,41 @@
 import { BigNumber } from "mathjs";
 import { gameMath } from "../gameMath";
 import { useGeneratorStore } from "../stores/generatorStore";
+import { useMemo } from "react";
 
 export const GENERATOR_MULTIPLIER_PER_LEVEL = gameMath.bignumber("1");
-export const GENERATOR_MULTIPLIER_COST_PER_LEVEL = gameMath.bignumber("10");
+export const GENERATOR_MULTIPLIER_BASE_COST = gameMath.bignumber("10");
+export const GENERATOR_MULTIPLIER_COST_RATE = gameMath.bignumber("3");
 export const GENERATOR_EXPONENT_PER_LEVEL = gameMath.bignumber("0.1");
-export const GENERATOR_EXPONENT_COST_PER_LEVEL = gameMath.bignumber("10");
+export const GENERATOR_EXPONENT_BASE_COST = gameMath.bignumber("10");
+export const GENERATOR_EXPONENT_COST_RATE = gameMath.bignumber("3");
 
 export function useGeneratorCalculation() {
   const generatorStore = useGeneratorStore();
 
-  const generatorExponent: BigNumber = gameMath.evaluate(
-    `1 + ((${generatorStore.exponentLevel} - 1) * ${GENERATOR_EXPONENT_PER_LEVEL})`
-  );
-  const generatorExponentCost: BigNumber = gameMath.evaluate(
-    `${generatorStore.exponentLevel} * ${GENERATOR_EXPONENT_COST_PER_LEVEL}`
-  );
-  const generatorMultiplier: BigNumber = gameMath.evaluate(
-    `${generatorStore.level} * ${GENERATOR_MULTIPLIER_PER_LEVEL}`
-  );
-  const generatorMultiplierCost: BigNumber = gameMath.evaluate(
-    `${generatorStore.level} * ${GENERATOR_MULTIPLIER_COST_PER_LEVEL}`
-  );
-  const generatorTickFactor: BigNumber = gameMath.evaluate(
-    `${generatorMultiplier} ^ ${generatorExponent}`
-  );
+  const generatorExponent: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `1 + ((${generatorStore.exponentLevel} - 1) * ${GENERATOR_EXPONENT_PER_LEVEL})`
+    );
+  }, [generatorStore.exponentLevel]);
+  const generatorExponentCost: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${GENERATOR_EXPONENT_BASE_COST} * (${GENERATOR_EXPONENT_COST_RATE}) ^ ${generatorStore.exponentLevel}`
+    );
+  }, [generatorStore.exponentLevel]);
+  const generatorMultiplier: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${generatorStore.level} * ${GENERATOR_MULTIPLIER_PER_LEVEL}`
+    );
+  }, [generatorStore.level]);
+  const generatorMultiplierCost: BigNumber = useMemo(() => {
+    return gameMath.evaluate(
+      `${GENERATOR_MULTIPLIER_BASE_COST} * (${GENERATOR_MULTIPLIER_COST_RATE}) ^ ${generatorStore.level}`
+    );
+  }, [generatorStore.level]);
+  const generatorTickFactor: BigNumber = useMemo(() => {
+    return gameMath.evaluate(`${generatorMultiplier} ^ ${generatorExponent}`);
+  }, [generatorExponent, generatorMultiplier]);
 
   return {
     generatorExponent,
